bb2d31192e9d95a3eb125924555847fde6fe4e93
Bruno Santos <brsantos197@gmail.com>
2023-04-18 13:05:27 -0300

    up: foi

--- src/components/WhatsApp/index.tsx
+++ src/components/WhatsApp/index.tsx
@@ -17,22 +17,28 @@ export const WhatsApp = ({ qrcode, ready, loading, disconnected }: WhatsAppProps
           <>
             {loading ? (
               <>
-                <h1 className="text-lg font-bold text-green-600">{ qrcode ? 'Conectando' : 'Carregando' }</h1>
+                <h1 className="text-lg font-bold text-green-600">{qrcode ? 'Conectando' : 'Carregando'}</h1>
                 <div className="animate-spin rounded-full border-4 border-t-teal-500  w-10 h-10 " ></div>
               </>
             ) :
               null
             }
-            {(qrcode && !loading) && !disconnected ? (
+            {(qrcode && !loading) ? (
               <>
-                <h1 className="text-lg font-bold text-green-600">
-                  Escaneie o QRCODE abaixo pelo aplicativo do WhatsApp com o seu celular!
-                </h1>
-                <QRCodeCanvas
-                  value={qrcode}
-                  size={256}
-                  fgColor={colors.teal['500']}
-                />
+                {
+                  !disconnected ? (
+                    <>
+                      <h1 className="text-lg font-bold text-green-600">
+                        Escaneie o QRCODE abaixo pelo aplicativo do WhatsApp com o seu celular!
+                      </h1>
+                      <QRCodeCanvas
+                        value={qrcode}
+                        size={256}
+                        fgColor={colors.teal['500']}
+                      />
+                    </>
+                  ) : null
+                }
               </>
             ) :
               null
