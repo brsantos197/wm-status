cb6ed7c7c30e39d71c2aa5305afbe8e69cbe631d
Bruno Santos <brsantos197@gmail.com>
2023-04-27 17:01:41 -0300

    no auto update

--- src/index.ts
+++ src/index.ts
@@ -13,43 +13,43 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Auto update
 
-const server = 'https://wm-status.vercel.app'
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
+// const server = 'https://wm-status.vercel.app'
+// const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
-autoUpdater.setFeedURL({ url })
+// autoUpdater.setFeedURL({ url })
 
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail:
-      'A new version has been downloaded. Restart the application to apply the updates.',
-  }
+// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: 'info',
+//     buttons: ['Restart', 'Later'],
+//     title: 'Application Update',
+//     message: process.platform === 'win32' ? releaseNotes : releaseName,
+//     detail:
+//       'A new version has been downloaded. Restart the application to apply the updates.',
+//   }
 
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
+//   })
+// })
 
-autoUpdater.on('checking-for-update', () => {
-  dialog.showErrorBox('Buscando', '...')
-})
+// autoUpdater.on('checking-for-update', () => {
+//   dialog.showErrorBox('Buscando', '...')
+// })
 
-autoUpdater.on('update-available', () => {
-  dialog.showErrorBox('Opa', 'Nova atualizaçao...')
-})
+// autoUpdater.on('update-available', () => {
+//   dialog.showErrorBox('Opa', 'Nova atualizaçao...')
+// })
 
-autoUpdater.on('error', (message) => {
-  dialog.showErrorBox('deu ruim', String(message))
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
+// autoUpdater.on('error', (message) => {
+//   dialog.showErrorBox('deu ruim', String(message))
+//   console.error('There was a problem updating the application')
+//   console.error(message)
+// })
 
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 1000 * 60)
+// setInterval(() => {
+//   autoUpdater.checkForUpdates()
+// }, 1000 * 60)
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
