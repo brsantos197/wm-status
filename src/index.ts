8f06f7071396e16b4a546daeb1eb18f940750776
Bruno Santos <brsantos197@gmail.com>
2023-04-28 10:58:08 -0300

    up: autoupdate

--- src/index.ts
+++ src/index.ts
@@ -11,46 +11,6 @@ import { resolve } from "path";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-// Auto update
-
-const server = 'https://wm-status.vercel.app'
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
-
-autoUpdater.setFeedURL({ url })
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Reiniciar e atualizar', 'Mais tarde'],
-    title: 'Atualização disponível!',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail:
-      'Uma nova versão foi baixada. Reinicie o aplicativo para aplicar as atualizações.',
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
-
-autoUpdater.on('checking-for-update', () => {
-  dialog.showErrorBox('Procurando', 'Buscando...')
-})
-
-autoUpdater.on('update-available', () => {
-  dialog.showErrorBox('Opa', 'Nova atualização')
-})
-
-autoUpdater.on('error', (message) => {
-  dialog.showErrorBox('deu ruim', String(message))
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
-
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 1000 * 60)
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -281,6 +241,43 @@ pie.initialize(app)
         }
       });
     }
+
+    // Auto update
+
+    const server = 'https://wm-status.vercel.app'
+    const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+    autoUpdater.setFeedURL({ url })
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+      const dialogOpts = {
+        type: 'info',
+        buttons: ['Reiniciar e atualizar', 'Mais tarde'],
+        title: 'Atualização disponível!',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail:
+          'Uma nova versão foi baixada. Reinicie o aplicativo para aplicar as atualizações.',
+      }
+
+      dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        mainWindow.removeAllListeners()
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+      })
+    })
+
+    autoUpdater.on('update-available', () => {
+      mainWindow.webContents.send('warn', 'Nova atualização disponível, baixando...')
+    })
+
+    autoUpdater.on('error', (message) => {
+      mainWindow.webContents.send('error', message)
+      console.error('There was a problem updating the application')
+      console.error(message)
+    })
+
+    setInterval(() => {
+      autoUpdater.checkForUpdates()
+    }, 1000 * 60)
 
   })
   .catch(err => console.error(err))
